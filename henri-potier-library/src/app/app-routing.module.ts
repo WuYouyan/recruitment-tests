5adba48c8820440d7339d74d05642329a49f1634
Youyan WU <wuyouyan1@gmail.com>
2020-03-28 17:27:15 +0100

    top-bar finished, global css changes and routes setting

--- henri-potier-library/src/app/app-routing.module.ts
+++ henri-potier-library/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BooksComponent} from "./books/books.component";
+import {ShoppingBasketComponent} from "./shopping-basket/shopping-basket.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'books' , pathMatch: 'full'},
+  {path: 'books', component: BooksComponent},
+  {path: 'shoppingbasket', component: ShoppingBasketComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
